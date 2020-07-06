35ba885d5f3341ac8931dbcbc4879a12faa71b62
jgaines2013 <jgaines2013@gmail.com>
2020-07-06 11:08:26 -0700

    Variable practice file

---
+++ variablePractice.py
@@ -0,0 +1,12 @@
+noun = "Unicorn"
+name = "John"
+age = 136
+varAge = 20
+print("There was a " + noun + " named " + name)
+print("his age was ", age)
+print("They would prefer to be", varAge, "years younger ")
+print("Their best years, were when they were ", (age - varAge))
+noun = "Gorilla"
+varAge = 30
+print("He also wanted to be a ", noun)
+print("but they only live ", varAge, " years")
